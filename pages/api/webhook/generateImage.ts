bb5fd29e24b9957baeb5aa5aa46e325536f99e07
tj <thinupavani@gmail.com>
2023-05-18 10:20:47 +0530

    image seed

--- pages/api/webhook/generateImage.ts
+++ pages/api/webhook/generateImage.ts
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 // {
@@ -23,26 +23,58 @@ import React from 'react'
 //     "responseAt": "2023-04-04T13:06:01.927Z"
 // }
 
-export default async function(req: { body: { userPrompt: string; }; }, res: any){
+export default async function (req: { body: { userPrompt: string; }; }, res: any) {
+
+    const [imageId, setImageId] = useState('')
     const userPrompt = req.body.userPrompt || '';
     console.log("data prompt :", userPrompt);
 
+
+    useEffect(() => {
+
+    }, [imageId])
+
+
     const response = await fetch('https://api.thenextleg.io/v2/imagine', {
         method: 'POST',
-        headers: { 
-            'Authorization': `Bearer ${process.env.NEXT_LEG}`, 
+        headers: {
+            'Authorization': `Bearer ${process.env.NEXT_LEG}`,
             'Content-Type': 'application/json'
-          },
-        body: JSON.stringify({ 
+        },
+        body: JSON.stringify({
             "msg": userPrompt
-         }),
+        }),
     });
 
     if (response.status !== 200) {
         const error = await response.json();
         throw new Error(error.message);
     }
-    
+
     const data = await response.json();
-    console.log("imagine : ",data)
+    console.log("imagine : ", data)
+    setImageId(data.messageId);
+
+    if (imageId) {
+        setTimeout(async () => {
+            const responseImage = await fetch(`https://api.thenextleg.io/v2/seed/${imageId}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${process.env.NEXT_LEG}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (responseImage.status !== 200) {
+                const error = await responseImage.json();
+                throw new Error(error.message);
+            }
+
+            const imageData = await responseImage.json();
+            console.log("image url : ",imageData.imageUrl);
+        }, 8000);
+    } else {
+        console.log('image id is null')
+    }
+
 }
